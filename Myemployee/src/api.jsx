640e0d58a4ef8a1db0cca134a22cf5d69e2490c9
siddhant2515 <siddhantjain658@gmail.com>
2025-01-02 13:41:42 +0530

    change base url

--- Myemployee/src/api.jsx
+++ Myemployee/src/api.jsx
@@ -1,5 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const GetAllEmployees = async (search = '', page = 1, limit = 5) => {
     const url =
