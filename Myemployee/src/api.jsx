e09658a3193e1793a3201ae7ac49b4f6020dd462
siddhant2515 <siddhantjain658@gmail.com>
2025-01-02 12:31:59 +0530

    Connect Frontend to Backend

--- Myemployee/src/api.jsx
+++ Myemployee/src/api.jsx
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
 
 export const GetAllEmployees = async (search = '', page = 1, limit = 5) => {
     const url =
